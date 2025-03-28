d532b8a0a6900153efa658b78f4ba5f2ea44d921
andrexduarte <andrexduarte@gmail.com>
2025-03-28 15:51:11 -0300

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,11 @@ export default defineNuxtConfig({
       cookie_flags: 'SameSite=None; Secure'
     }
   },
-  
+
   i18n: {
     lazy: true,
-    langDir: "locales", 
-    strategy: "prefix_except_default",
+    langDir: 'locales',
+    strategy: 'prefix_except_default',
     locales: [
       { code: 'en', language: 'en-US', name: 'English', file: 'en.json' },
       { code: 'pt-br', language: 'pt-BR', name: 'Português (Brasil)', file: 'pt-br.json' }
@@ -26,21 +26,21 @@ export default defineNuxtConfig({
     detectBrowserLanguage: {
       useCookie: true,
       cookieKey: 'i18n_redirected',
-      alwaysRedirect: false,  // Não redireciona automaticamente com base no idioma do navegador
-      fallbackLocale: 'pt-br',  // Caso não haja cookie, usar pt-br
+      alwaysRedirect: false,
+      fallbackLocale: 'pt-br',
     }
   },
 
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/', // baseURL: '/<repository>/'
+    baseURL: '/',
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'icon', type: 'image/png', href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon-96x96.png`, sizes: '96x96' },
-        { rel: 'icon', type: 'image/svg+xml', href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon.svg` },
-        { rel: 'shortcut icon', href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon.ico` },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: `${process.env.NUXT_APP_BASE_URL || '/'}apple-touch-icon.png` }
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
       ],
       meta: [
         { name: 'apple-mobile-web-app-title', content: 'André Duarte - Front-end developer' },
@@ -50,13 +50,13 @@ export default defineNuxtConfig({
   },
 
   vite: {
-      css: {
-          preprocessorOptions: {
-              scss: {
-                  api: "modern-compiler"
-              }
-          }
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        }
       }
+    }
   },
 
   image: {
@@ -67,7 +67,6 @@ export default defineNuxtConfig({
       xl: 1280,
     }
   },
-  
 
   build: {
     transpile: ['@nuxtjs/color-mode'],
